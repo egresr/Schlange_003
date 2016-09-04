46539e17994a22015565cce7219179d02c8df3e1
rs sr <rsdaten@live.de>
2016-09-04 13:42:50 +0200

    -- Class LinieHorisontal __OK__ -- Class LinieVertikal __OK__

--- pnSchlange3/nSchlange3/Program.cs
+++ pnSchlange3/nSchlange3/Program.cs
@@ -13,9 +13,17 @@ namespace nSchlange3
             Point point = new Point(3, 3, '+');
             point.Zeichnen();
 
+            LinieHorizontal linieHorizontalOben = new LinieHorizontal(0, 0, 5, 'H');
+            LinieHorizontal linieHorizontalUnten = new LinieHorizontal(0, 4, 5, 'H');
+            LinieVertikal linieVertikalLinks = new LinieVertikal(0, 0, 5, 'v');
+            LinieVertikal linieVertikalRechts = new LinieVertikal(4, 0, 5, 'v');
+            linieHorizontalOben.Zeichnen();
+            linieHorizontalUnten.Zeichnen();
+            linieVertikalLinks.Zeichnen();
+            linieVertikalRechts.Zeichnen();
 
 
             Console.ReadLine();
         }
-    }
+    } 
 }
