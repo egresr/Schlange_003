59aed70b8ce33f13ad93ad17229b9e8a30c46bf0
rs sr <rsdaten@live.de>
2016-09-04 23:06:13 +0200

    -- Kollision Schlange / Nahrung, Nahrung neu erzeugen.

---
+++ pnSchlange3/nSchlange3/Nahrung.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nSchlange3
+{
+    class Nahrung
+    {
+        Random random = new Random();    
+
+        public Point Erzeugen(SpielFeld spielfeld, char nahrungSymbol)
+        {
+            int maxPositionX = spielfeld.GetBreite();
+            int maxPositionY = spielfeld.GetHohe();
+            int randomPositionX = random.Next(1, maxPositionX);
+            int randomPoitionY = random.Next(1, maxPositionY);
+            return new Point(randomPositionX, randomPoitionY, nahrungSymbol);
+        }
+    }
+}
